c47d89ff91dcdabdfa52a570fec5c69c2b653a22
Jack Tyler <jacktyler2391@gmail.com>
2026-05-18 15:52:35 +0300

    add missing file

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+from calculator import Calculator
+
+
+@pytest.fixture(scope="session")
+def calculator():
+    """
+    Provide a valid calculator instance for whole test session.
+    :return: Calculator instance
+    """
+    calculator = Calculator()
+    yield calculator
+
+    del calculator
